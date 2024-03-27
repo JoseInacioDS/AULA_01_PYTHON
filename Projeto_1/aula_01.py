aa33b12fb722ecf82c3e06ce83682e7add23f2d0
José Inácio Bitencourt da Silva <129962133+JoseInacioDS@users.noreply.github.com>
2024-03-27 10:49:58 -0300

    Add files via upload

---
+++ Projeto_1/aula_01.py
@@ -0,0 +1,11 @@
+import os
+
+print("Bom Dia")
+print("o 3DS é a turma Numero 1 do Sagrada")
+turma ="3DS"
+pos = 2
+print("o", turma, "é a turma Numero", pos, "do Sagrada")
+print(type(turma))
+print(type(pos))
+
+os.system("clear")
